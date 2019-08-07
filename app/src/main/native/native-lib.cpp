f78da763e3f8bb049e728ee37a05b299629780ed
U-dq55-000044z70\Administrator <dq55-000044z70+Administrator@DQ55-000044Z70.it2004.gree.com.cn>
2019-08-07 20:23:38 +0800

    20180901

---
+++ app/src/main/native/native-lib.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Administrator on 2019/8/7 0007.
+//
+#include "libyuv.h"
+#include <string>
+//#include <opencv2/opencv.hpp>
+#include "com_zkteco_autk_utils_ImageUtil.h"
+
+//using namespace cv;
+
+
+extern "C"
+
+JNIEXPORT jboolean JNICALL Java_com_zkteco_autk_utils_ImageUtil_absdiff
+        (JNIEnv *, jclass, jbyteArray , jbyteArray) {
+    I420Scale()
+    https://codeday.me/bug/20171113/95862.html
+    https://github.com/eterrao/AndroidLibyuvImageUtils/blob/master/app/src/main/jni/tech_shutu_jni_YuvUtils.c
+    return true;
+}
